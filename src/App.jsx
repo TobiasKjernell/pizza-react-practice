cc5da75da3c10b773510cf1b0d9a07370d779729
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-07-10 19:23:37 +0200

    added data fetching in router on load.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from "./features/menu/Menu"
+import Menu, { menuLoader } from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
 import Order from "./features/order/Order"
 import CreateOrder from "./features/order/CreateOrder"
@@ -17,7 +17,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/menu',
-        element: <Menu />
+        element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: '/cart',
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,18 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  
+  return <ul>
+    {menu.map((pizza) => <MenuItem key={pizza.id} pizza={pizza} />)}
+  </ul>
+}
+
+export const menuLoader = async () => {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
